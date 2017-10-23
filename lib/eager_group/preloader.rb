962c8f493de5d6bb7c70207a971d906dededc643
happyming9527 <happyming9527@gmail.com>
2017-10-23 04:28:36 -0500

    preload的primary key不再只取primary_id

--- lib/eager_group/preloader.rb
+++ lib/eager_group/preloader.rb
@@ -8,15 +8,15 @@ module EagerGroup
 
     # Preload aggregate functions
     def run
-      primary_key = @klass.primary_key
-      record_ids = @records.map { |record| record.send primary_key }
       @eager_group_values.each do |eager_group_value|
         defination_key, arguments = eager_group_value.is_a?(Array) ? [eager_group_value.shift, eager_group_value] : [eager_group_value, nil]
         if definition = @klass.eager_group_definations[defination_key]
-          reflection = @klass.reflect_on_association(definition.association)
-          association_class = reflection.klass
-          association_class = association_class.instance_exec(*arguments, &definition.scope) if definition.scope
-          polymophic_as_condition = lambda {|reflection|
+          reflection              = @klass.reflect_on_association(definition.association)
+          primary_key = reflection.options[:primary_key] || @klass.primary_key
+          record_ids  = @records.map { |record| record.send primary_key }
+          association_class       = reflection.klass
+          association_class       = association_class.instance_exec(*arguments, &definition.scope) if definition.scope
+          polymophic_as_condition = lambda { |reflection|
             if reflection.type
               ["#{reflection.name}.#{reflection.type} = ?", @klass.base_class.name]
             else
@@ -25,17 +25,17 @@ module EagerGroup
           }
 
           if reflection.through_reflection
-            foreign_key = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
+            foreign_key    = "#{reflection.through_reflection.name}.#{reflection.through_reflection.foreign_key}"
             aggregate_hash = association_class.joins(reflection.through_reflection.name)
-                                              .where("#{foreign_key} IN (?)", record_ids)
-                                              .where(polymophic_as_condition.call(reflection.through_reflection))
-                                              .group("#{foreign_key}")
-                                              .send(definition.aggregate_function, definition.column_name)
+                               .where("#{foreign_key} IN (?)", record_ids)
+                               .where(polymophic_as_condition.call(reflection.through_reflection))
+                               .group("#{foreign_key}")
+                               .send(definition.aggregate_function, definition.column_name)
           else
             aggregate_hash = association_class.where(reflection.foreign_key => record_ids)
-                                              .where(polymophic_as_condition.call(reflection))
-                                              .group(reflection.foreign_key)
-                                              .send(definition.aggregate_function, definition.column_name)
+                               .where(polymophic_as_condition.call(reflection))
+                               .group(reflection.foreign_key)
+                               .send(definition.aggregate_function, definition.column_name)
           end
           @records.each do |record|
             id = record.send primary_key
